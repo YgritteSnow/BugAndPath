fd1ea40d850fe72256fb44b06b049e9c3a85e93f
YgritteSnow <773963715@qq.com>
2018-03-20 08:40:50 +0800

    backup

---
+++ Assets/AniPath/Scripts/AntMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SLua;
+
+public class AntMove : MonoBehaviour {
+	public float forward_speed = 0.0f;
+	public float rotate_speed = 0.0f;
+
+	public float total_time = 1;
+
+	private LuaState L = null;
+	private LuaFunction lua_func_rotation = null;
+	private LuaFunction lua_func_position = null;
+	public OperatorCodeGen rotation_code = null;
+	public OperatorCodeGen position_code = null;
+
+	void Update ()
+	{
+		if(lua_func_rotation == null || lua_func_position == null)
+		{
+			return;
+		}
+		float time = Time.fixedTime % total_time;
+		rotate_speed = float.Parse(lua_func_rotation.call(new object[1] { time }).ToString());
+		forward_speed = float.Parse(lua_func_position.call(new object[1] { time }).ToString());
+
+		float delta_time = Time.deltaTime;
+
+		Vector3 old_position = transform.position;
+		Vector3 new_position = old_position + forward_speed * delta_time * transform.TransformDirection(Vector3.up);
+		new_position.z = -1;
+
+		Quaternion old_rotation = transform.rotation;
+		Quaternion new_rotation = old_rotation * Quaternion.Euler(new Vector3(0, 0, delta_time * rotate_speed));
+
+		transform.SetPositionAndRotation(new_position, new_rotation);
+	}
+
+	static private AntMove _instance = null;
+	static public AntMove Instance { get { return _instance; } }
+	private void Awake()
+	{
+		_instance = this;
+		L = new LuaState();
+		ResetFunc();
+	}
+
+	public void ResetFunc()
+	{
+		string rot_str = rotation_code.genCode();
+		string pos_str = position_code.genCode();
+		L.doString("function RotationFunc(time) \n" + rot_str + "\n end");
+		L.doString("function PositionFunc(time) \n" + pos_str + "\n end");
+		lua_func_rotation = L.getFunction("RotationFunc");
+		lua_func_position = L.getFunction("PositionFunc");
+	}
+}
---
+++ Assets/AniPath/Scripts/DrawLines.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLines : MonoBehaviour
+{
+	string propertyName = "_Blend_Texture";
+	public Texture2D brushTexture;
+
+	public int width = 512;
+	public int height = 512;
+
+	Material mat;
+	Texture2D maskTexture;
+	
+	public int brushWidth;
+	public int brushHeight;
+	int lastCenterX = -1;
+	int lastCenterY = -1;
+
+	bool hasStarted = false;
+
+	void Awake()
+	{
+		if (GetComponent<Renderer>() != null)
+		{
+			mat = GetComponent<Renderer>().material;
+		}
+
+		Debug.Log("DrawLine: shader name = " + mat.shader.name);
+		maskTexture = new Texture2D(width, height, TextureFormat.Alpha8, false);
+		maskTexture.wrapMode = TextureWrapMode.Clamp;
+		mat.SetTexture(propertyName, maskTexture);
+
+		ClearDraw();
+	}
+
+	Color[] PureColors(int width, int height, Color color)
+	{
+		Color[] result = new Color[width * height];
+		for (int i = 0; i < result.Length; i++)
+		{
+			result[i] = color;
+		}
+		return result;
+	}
+
+	public void ClearDraw()
+	{
+		maskTexture.SetPixels(PureColors(maskTexture.width, maskTexture.height, Color.black));
+		maskTexture.Apply();
+		hasStarted = false;
+	}
+
+	public void DrawAt(Vector3 new_pos)
+	{
+		if(hasStarted)
+		{
+			DrawLine(new_pos);
+		}
+		else
+		{
+			DrawDot(new_pos);
+		}
+		//hasStarted = true;
+	}
+
+	public void DrawDot(Vector3 new_pos)
+	{
+		Color[] colors = brushTexture.GetPixels();
+		maskTexture.SetPixels((int)new_pos.x - brushWidth / 2, (int)new_pos.y - brushHeight / 2, brushWidth, brushHeight, colors);
+	}
+
+	public void DrawLine(Vector3 new_pos)
+	{
+	}
+}
